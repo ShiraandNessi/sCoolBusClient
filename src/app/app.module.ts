e31fffbb9482a14ef7a3ff7411f27cd3a30acf86
ShiraandNessi <97845269+ShiraandNessi@users.noreply.github.com>
2022-02-08 16:31:39 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserModule } from './modules/user/user.module';
+import { HttpClientModule } from '@angular/common/http'
+import { LogInComponent } from './modules/user/log-in/log-in.component';
+import { Route, RouterModule } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
+
+const APP_ROUTES:Route[]=[
+  {path:"", pathMatch: "full", redirectTo: "user"},
+  {path:"user",component:LogInComponent},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
+    HttpClientModule,
+    RouterModule.forRoot(APP_ROUTES)
+  ],
+  providers: [MessageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/student.model.ts
@@ -0,0 +1,14 @@
+export class Student
+{
+    id!:number;
+    familyId!:number;
+    firstName!:string;
+    lastName!:string;
+    grade!:number;
+    phone!:string;
+    passport!:string;
+    routeId!: number;
+    imageRoute!:string;
+    
+
+}
