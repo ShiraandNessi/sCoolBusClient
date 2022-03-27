c719ad6992e24cd8c471a182ca5bac55f02ddf4a
Nessi Hutzler <213087448@PC5-3>
2022-03-27 20:08:29 +0300

    27.3-good night

--- src/app/modules/family/family-map/family-map.component.ts
+++ src/app/modules/family/family-map/family-map.component.ts
@@ -11,75 +11,80 @@ export class FamilyMapComponent implements OnInit {
   title = 'my-maps-project';
   zoom = 12
   center!: google.maps.LatLngLiteral
-  markers=[] as any
-  directionsService =[] as any
-  directionsRenderer= [] as any
-  stationsList!:Station[];
-  map :google.maps.Map | undefined;
-  constructor(private station: StationService){}
+  markers = [] as any
+  directionsService = [] as any
+  directionsRenderer = [] as any
+  stationsList!: Station[];
+  map: google.maps.Map | undefined;
+  image = {
+    url: ".\.\.\.\assets\location-pin.png",
+    size: new google.maps.Size(20, 32)
+  }
+  constructor(private station: StationService) { }
   ngOnInit() {
-this.station.getAllStations().subscribe(data=>{
-  this.stationsList=data,
-  navigator.geolocation.getCurrentPosition((position) => {
-    this.center = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude,
-    },
-    this.markers[0]={
-      position: {
-        lat: this.center.lat,
-        lng: this.center.lng,
-      },
-      label: {
-        color: 'red',
-        text: 'you are here',
-      },
-      title: 'Family Home ',
-      options: { animation: google.maps.Animation.BOUNCE },
-      setClickable:true
-    },
-    this.directionsService = new google.maps.DirectionsService(),
-    this.directionsRenderer = new google.maps.DirectionsRenderer();
-     this.map = new google.maps.Map(
-      document.getElementById("map") as HTMLElement,
-      {
-        zoom: 6,
-        center: { lat: 41.85, lng: -87.65 },
-      }
-    );
-  
-    this.directionsRenderer.setMap(this.map);
-  
-    
-  })
-  this.stationsList.forEach((s,i)=>{
-    this.markers.push({
-    position: {
-      lat: s.pointX,
-      lng: s.pointY,
-    },
-    label: {
-      color: 'red',
-      text: s.address,
-    },
-    title: s.address,
-    setClickable:true
-  })
-  // google.maps.event.addListener(this.markers[i], 'click', function() {console.log("ggggggggggggggggggg");});
+    this.station.getAllStations().subscribe(data => {
+      this.stationsList = data,
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          },
+            this.markers[0] = {
+              position: {
+                lat: this.center.lat,
+                lng: this.center.lng,
+              },
+              label: {
+                color: 'red',
+                text: 'you are here',
+              },
+              title: 'Family Home ',
+              options: { animation: google.maps.Animation.BOUNCE },
+              setClickable: true
+            },
+            this.directionsService = new google.maps.DirectionsService(),
+            this.directionsRenderer = new google.maps.DirectionsRenderer();
+          this.map = new google.maps.Map(
+            document.getElementById("map") as HTMLElement,
+            {
+              zoom: 6,
+              center: { lat: 41.85, lng: -87.65 },
+            }
+          );
 
-  })
-  const infoWindow = new google.maps.InfoWindow({
-    content: "",
-    disableAutoPan: true,
-  });
-     // markers can only be keyboard focusable when they have click listeners
-    // open info window when marker is clicked
-    this.markers[0].addListener("click", () => {
-      infoWindow.setContent(this.markers[0].label);
-      infoWindow.open(this.map, this.markers[0]);
-    });
-})
-    
+          this.directionsRenderer.setMap(this.map);
+
+
+        })
+      this.stationsList.forEach((s, i) => {
+        this.markers.push({
+          position: {
+            lat: s.pointX,
+            lng: s.pointY,
+          },
+          label: {
+            color: 'black',
+            text: s.address,
+          },
+          icon: this.image,
+          title: s.address,
+          setClickable: true
+        })
+        // google.maps.event.addListener(this.markers[i], 'click', function() {console.log("ggggggggggggggggggg");});
+
+      })
+      const infoWindow = new google.maps.InfoWindow({
+        content: "",
+        disableAutoPan: true,
+      });
+      // markers can only be keyboard focusable when they have click listeners
+      // open info window when marker is clicked
+      this.markers[0].addListener("click", () => {
+        infoWindow.setContent(this.markers[0].label);
+        infoWindow.open(this.map, this.markers[0]);
+      });
+    })
+
   }
 
 
